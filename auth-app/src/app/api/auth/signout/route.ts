bb74499220831cbe88be1f62d60631aeb5ac75e1
Diego Engelhard <00356119@uca.edu.sv>
2024-02-23 10:21:07 -0600

    Add: sign out from server

---
+++ auth-app/src/app/api/auth/signout/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+// Sign Out function
+export async function GET() {
+    try {
+        // Set success message
+        const response = NextResponse.json({
+            message: 'Sign out successful',
+            success: true,
+        });
+
+        // Remove token from cookie
+        response.cookies.set('token', '', {
+            httpOnly: true,
+            expires: new Date(0),
+        });
+
+        // Return respones
+        return response;
+    } catch (error: any) {
+        console.log(error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
